3f31ea113537776df871d308a5c49c21e2e68e5f
Stefan Schindler <stefan@estada.ch>
2015-11-12 21:22:25 +0100

    Poorly handle failer of telegram-api library

--- src/main.rs
+++ src/main.rs
@@ -8,85 +8,89 @@ use rustc_serialize::json::Json;
 use std::io::{Read};
 
 fn main() {
-    // Create bot, test simple API call and print bot information
-    let api = Api::from_env("TELEGRAM_BOT_TOKEN").unwrap();
-    println!("getMe: {:?}", api.get_me());
-    let mut listener = api.listener(ListeningMethod::LongPoll(None));
-    
-    let path_to_picture :String = "rust-logo-blk.png".to_string();
-    let caption_to_picture :String = "Rust Logo".to_string();
+    loop {
+        // Create bot, test simple API call and print bot information
+        let api = Api::from_env("TELEGRAM_BOT_TOKEN").unwrap();
+        println!("getMe: {:?}", api.get_me());
+        let mut listener = api.listener(ListeningMethod::LongPoll(None));
+        
+        let path_to_picture :String = "rust-logo-blk.png".to_string();
+        let caption_to_picture :String = "Rust Logo".to_string();
 
-    // Fetch new updates via long poll method
-    let res = listener.listen(|u| {
-        // If the received update contains a message...
-        if let Some(m) = u.message {
-            let name = m.from.first_name;
+        // Fetch new updates via long poll method
+        let res = listener.listen(|u| {
+            // If the received update contains a message...
+            if let Some(m) = u.message {
+                let name = m.from.first_name;
 
-            // Match message type
-            match m.msg {
-                MessageType::Text(t) => {
-                    // Print received text message to stdout
-                    println!("<{}> {}", name, t);
-                    let t = t.replace("@CoreDumpBot", "");
-                    let ts:&str = t.trim();
-                    
-                    match ts {
-                      "/getPicture" | "/get_picture" => { 
-                      try!(api.send_photo(
+                // Match message type
+                match m.msg {
+                    MessageType::Text(t) => {
+                        // Print received text message to stdout
+                        println!("<{}> {}", name, t);
+                        let t = t.replace("@CoreDumpBot", "");
+                        let ts:&str = t.trim();
+                        
+                        match ts {
+                        "/getPicture" | "/get_picture" => { 
+                        try!(api.send_photo(
+                                    m.chat.id(),
+                                    path_to_picture.clone(), // Path
+                                    Some(caption_to_picture.clone()), // caption
+                                    None, // reply_to_message_id
+                                    None  // reply_markup
+                            ));
+                        },
+                        "/help" => {
+                            try!(api.send_message(
+                                    m.chat.id(),
+                                    format!("No such help 😜\nuse /getPicture for a snapshot of the 3d printer.\nuse /crowd for an update on people now present"),
+                                    None, None, None
+                            ));
+                        },
+                        "/crowd" => {
+                            let s = match fetch_people_now_present() {
+                            Ok(people_now_present) if people_now_present > 1 => format!("{} people are present!", people_now_present),
+                            Ok(people_now_present) if people_now_present == 1 => format!("One person is present!"),
+                            Ok(_) => format!("Nobody here right now."),
+                            Err(e) => format!("An error occured 😕\n{}", e),
+                            };
+                            try!(api.send_message(
+                                    m.chat.id(),
+                                    s,
+                                    None, None, None
+                            ));
+                        },
+                        "/start" => {
+                            try!(api.send_message(
+                                    m.chat.id(),
+                                    format!("Welcome to CoredumpBot\nuse /getPicture for a snapshot of the 3d printer."),
+                                    None, None, None
+                            ));
+                        },
+                        _ => { /* ignore */ }, 
+                        }
+                    },
+                    _ => {
+                        try!(
+                            api.send_message(
                                 m.chat.id(),
-                                path_to_picture.clone(), // Path
-                                Some(caption_to_picture.clone()), // caption
-                                None, // reply_to_message_id
-                                None  // reply_markup
-                        ));
-                      },
-                      "/help" => {
-                        try!(api.send_message(
-                                  m.chat.id(),
-                                  format!("No such help 😜\nuse /getPicture for a snapshot of the 3d printer.\nuse /crowd for an update on people now present"),
-                                  None, None, None
-                        ));
-                      },
-                      "/crowd" => {
-                        let s = match fetch_people_now_present() {
-                          Ok(people_now_present) if people_now_present > 1 => format!("{} people are present!", people_now_present),
-                          Ok(people_now_present) if people_now_present == 1 => format!("One person is present!"),
-                          Ok(_) => format!("Nobody here right now."),
-                          Err(e) => format!("An error occured 😕\n{}", e),
-                        };
-                        try!(api.send_message(
-                                  m.chat.id(),
-                                  s,
-                                  None, None, None
-                        ));
-                      },
-                      "/start" => {
-                        try!(api.send_message(
-                                  m.chat.id(),
-                                  format!("Welcome to CoredumpBot\nuse /getPicture for a snapshot of the 3d printer."),
-                                  None, None, None
-                        ));
-                      },
-                      _ => { /* ignore */ }, 
+                                format!("Unknown Command ..."),
+                                None, None, None)
+                        );
                     }
-                },
-                _ => {
-                    try!(
-                        api.send_message(
-                            m.chat.id(),
-                            format!("Unknown Command ..."),
-                            None, None, None)
-                    );
                 }
             }
+
+            // If none of the "try!" statements returned an error: It's Ok!
+            Ok(ListeningAction::Continue)
+        });
+
+        if let Err(e) = res {
+            println!("An error occured: {}", e);
+            // Rest for 10 Seconds
+            std::thread::sleep_ms(10 * 1000);
         }
-
-        // If none of the "try!" statements returned an error: It's Ok!
-        Ok(ListeningAction::Continue)
-    });
-
-    if let Err(e) = res {
-        println!("An error occured: {}", e);
     }
 }
 
@@ -94,12 +98,12 @@ fn fetch_people_now_present() -> std::result::Result<i64, String> {
   let client = Client::new();
 
   match client.get("https://status.coredump.ch/").send() {
-    Err(e) => Err(format!("{}", e)),
+    Err(e) => Err(format!("client.get() error:\n{}", e)),
     Ok(mut res) => {
       
       let mut body = String::new();
       match res.read_to_string(&mut body) {
-        Err(e) => { Err(format!("unable to connect to server, try again later\n{}", e)) },
+        Err(e) => { Err(format!("unable to connect to server, try again later:\n{}\n{}", e, body)) },
         Ok(_/*len*/) => {
           
           match Json::from_str( &*body ) {
