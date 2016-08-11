6a91a8b45e3db61b732c4fa38ed53f1c2ad6660d
Stefan Schindler <stefan@estada.ch>
2016-08-11 16:56:48 +0200

    Include grammer at compile time

--- src/grammar.rs
+++ src/grammar.rs
@@ -6,44 +6,31 @@ use std::fs::File;
 
 /// Returns everything from "src/user_input_compiler.rs" util it passes an empty Line.
 pub fn get_grammar_string() -> String {
-  let f = match File::open("src/user_input_compiler.rs") {
-    Ok(f) => f,
-    Err(e) => return format!("grammar::open error: {:?}", e),
-  };
-  let mut reader = BufReader::new(f);
+  let file = include_str!("../src/user_input_compiler.rs");
   let mut grammar = String::new();
   let mut max_lines_to_read = 100;
   let mut skipping_header = true;
   
-  loop {
-    let mut buffer = String::new();
-    // read a line into buffer
-    match reader.read_line(&mut buffer) {
-      Ok(_) => {},
-      Err(e) => return format!("grammar::read_line error: {:?}", e),
-    }
-
-
-    if buffer == "//! ```\n" {
-      skipping_header = false;
-    }
-
+  for line in file.split('\n') {
     if max_lines_to_read > 0 {
       max_lines_to_read -= 1;
     } else {
       break;
     }
-
+    
+    if line == "//! ```" {
+      skipping_header = false;
+    }
     if skipping_header {
       continue;
     }
 
-    if buffer == "\n" { // Block Change
+    if line == "" { // Block Change
       return grammar;
     }
 
-    let (_, line) = buffer.split_at(4);
-    grammar = grammar + line;
+    let (_, line) = line.split_at(4);
+    grammar = grammar + line + "\n";
   }
   
   grammar
